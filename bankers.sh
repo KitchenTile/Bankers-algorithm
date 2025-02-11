dd4fbdb8f4b43fb5a540c1e5e7d5af0a017d7e3e
Blue <118444162+KitchenTile@users.noreply.github.com>
2025-02-11 09:06:00 +0000

    Add files via upload

--- bankers.sh
+++ bankers.sh
@@ -4,14 +4,14 @@
 
 #these arrays will be filled by user
 available=()
-max_need=()
+maxNeed=()
 allocated=()
 
-#this array will be calculated with max_need - allocated
+#this array will be calculated with maxNeed - allocated
 need=()
 
 #this array will be filled after each process runs succesfully
-safe_sequence=()
+safeSequence=()
 
 # since I use the index calculation often I wrote it as a function
 index() {
@@ -38,40 +38,38 @@ read -p "Enter number of processes: " processes
 
 
 # available resources
-echo "Enter available resources (space-separated):"
+echo "Enter available resources (separated ny spaces):"
 read -ra available
 
 # max need matrix
-echo "Enter the maximum resource need matrix (row by row):"
+echo "Enter the maximum resource need matrix (row):"
 for ((i = 0; i < processes; i++)); do
     for ((j = 0; j < resources; j++)); do
         idx=$(index $i $j)
         read -p "Max Need [$i,$j]: " value
-         max_need[$idx]=$value
+         maxNeed[$idx]=$value
     done
 done
 
 
 # allocated resources matrix
-echo "Enter the allocated resources matrix (row by row):"
+echo "Enter the allocated resources matrix (row):"
 for ((i = 0; i < processes; i++)); do
     for ((j = 0; j < resources; j++)); do
         idx=$(index $i $j)
         read -p "Allocated [$i,$j]: " value
         allocated[$idx]=$value
-
-        # Compute the Need matrix dynamically
-        need[$idx]=$(( max_need[$idx] - allocated[$idx] ))
+        need[$idx]=$(( maxNeed[$idx] - allocated[$idx] ))
     done
 done
 
 # MATRICES DISPLAY
 
 echo -e "\nMax need matrix: "
-# display_matrix max_need
+# display_matrix maxNeed
 for ((i = 0; i < processes; i++)); do
     for ((j = 0; j < resources; j++)); do
-        echo -n "${max_need[$(index $i $j)]} "
+        echo -n "${maxNeed[$(index $i $j)]} "
     done
     echo
 done
@@ -105,28 +103,34 @@ for ((i = 0; i < processes; i++)); do
     finish[i]=0; 
 done
 
-found_process=true
+# implementing two flags, process flag keeps track of potential processes to try to execute
+processFlag=true
 
-while $found_process; do
-    found_process=false
+# we start it as true to start the loop
+while $processFlag; do
+    # and immediately set it as false so if there are no remaining possible processes to execute, exit the loop
+    processFlag=false
     for ((p = 0; p < processes; p++)); do
         if ((finish[p] == 0)); then
             echo "checking process number: $(($p+1))"
-            can_allocate=true
-            #go through resources and compare matrices
+            # allocation flag is truthy by default when we start to check a process, 
+            # it checks processes that can be executed and it's resources allocated
+            allocationFlag=true
+            # here we go through resources and compare matrices
             for ((r = 0; r < resources; r++)); do
                 idx=$(index $p $r)
                 if ((need[$idx] > work[r])); then
-                    #if the need is larger than the available resource, flag it as unable to be allocated to current work and move to next process
-                    can_allocate=false
+                    # if the need is larger than the available resource, flag it as unable to be allocated to current work and move to next process
+                    allocationFlag=false
                     echo "current available resources are not enough to succesfully go through process number: $(($p+1))"
                     break
                 fi
             done
 
-            if $can_allocate; then
+            # if the process can be executed
+            if $allocationFlag; then
                 echo "Process $(($p+1)) passed the resource check,"
-                #go through the resources of the process that was succesfully assessed
+                # go through its resources
                 for ((r = 0; r < resources; r++)); do
                     idx=$(index $p $r)
                     echo "adding processes allocated resources to current work -> resource number $(($r+1)) (${work[r]} + ${allocated[$idx]})"
@@ -137,7 +141,7 @@ while $found_process; do
                 echo "${work[@]}"
                 finish[p]=1
                 safe_sequence+=("$p")
-                found_process=true
+                processFlag=true
             fi
         fi
     done
